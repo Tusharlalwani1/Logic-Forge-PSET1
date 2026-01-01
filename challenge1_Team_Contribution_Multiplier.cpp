20639e7d7ac7c6c7febff571d1a3716a5a9495a9
Tushar_lalwani <lalwanit34@gmail.com>
2026-01-01 18:31:09 +0500

    solution for Challenge 1: Team Contribution Multiplier

---
+++ challenge1_Team_Contribution_Multiplier.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+vector<int> teamContributionMultiplier(const vector<int>& contributions){
+    int n = contributions.size();
+    vector<int> impact(n, 1);
+
+    // prod elements ka left side se
+    int leftProd = 1;
+    for(int i = 0; i < n; i++){
+        impact[i] = leftProd;
+        leftProd *= contributions[i];
+    }
+
+    // right side se prod
+
+    int rightProd = 1;
+    for(int i = n - 1; i>= 0; i--){
+        impact[i] *= rightProd;
+        rightProd *= contributions[i];
+    }
+
+    return impact;
+
+}
+
+int main()
+{
+    vector<int> contributions = {1, 2, 3, 4};
+    vector<int> result = teamContributionMultiplier(contributions);
+
+    cout << "Impact array: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
