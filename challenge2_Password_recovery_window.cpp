f23cff1af0b7614d2a2a22f249adfc155b9d77f6
Tushar_lalwani <lalwanit34@gmail.com>
2026-01-03 18:53:45 +0500

    Solution of challenge 2 minimum window

---
+++ challenge2_Password_recovery_window.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <unordered_map>
+#include <climits>
+#include <string>
+
+using namespace std;
+
+string minWindow(string s, string t) {
+    int n = s.length();
+
+    if (t.length() > n) {
+        return "";
+    }
+
+    unordered_map<char, int> mp;
+
+    // store pattern frequency
+    for (char ch : t) {
+        mp[ch]++;
+    }
+
+    int requiredCount = t.length();
+    int i = 0, j = 0;
+
+    int minWindowSize = INT_MAX;
+    int start_i = 0;
+
+    while (j < n) {
+        char ch = s[j];
+
+        if (mp[ch] > 0)
+            requiredCount--;
+
+        mp[ch]--;
+
+        while (requiredCount == 0) {
+            int currentWindowSize = j - i + 1;
+
+            if (minWindowSize > currentWindowSize) {
+                minWindowSize = currentWindowSize;
+                start_i = i;
+            }
+
+            mp[s[i]]++;
+
+            if (mp[s[i]] > 0) {
+                requiredCount++;
+            }
+
+            i++;
+        }
+        j++;
+    }
+
+    return minWindowSize == INT_MAX ? "" : s.substr(start_i, minWindowSize);
+}
